35d9c3863ed8007896a924d6a855faed60099fd7
Alex Sartori <alex.sartori1997@gmail.com>
2021-12-23 17:58:59 +0100

    Fix hour modulo offset

--- wordclock.js
+++ wordclock.js
@@ -13,7 +13,7 @@ $.fn.wordClock = function() {
         $('#wc-it').addClass('wc-clock-label-active');
         $('#wc-is').addClass('wc-clock-label-active');
 
-        var h = new Date().getHours() % 13;
+        var h = (new Date().getHours() - 1) % 12 + 1;
         var m = new Date().getMinutes();
 
         if (m <= 57 && m > 3) {
